b0134db63a7d817adc181a179f2135bbb47c310c
Wlad <283005@student.pwr.edu.pl>
2024-10-09 13:10:47 +0200

    Added new line

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int size = 20; // Rozmiar tablicy
         //stas sosulka
+        //stas krutaja sosulka
         int numberOfCreatures = 7; // Liczba istot każdego typu
         (new Simulation(size, numberOfCreatures)).run();
     }
