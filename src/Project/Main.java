89bf3e99a8599ba2d2902e4b5dbf45fd6d45f318
Wlad <283005@student.pwr.edu.pl>
2024-10-09 13:26:21 +0200

    Stas czto-to deleat

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -3,6 +3,7 @@ package Project;
 public class Main {
     public static void main(String[] args) {
         //stas
+        //,y jebemsy
         int size = 20; // Rozmiar tablicy
         int numberOfCreatures = 7; // Liczba istot każdego typu
         (new Simulation(size, numberOfCreatures)).run();
