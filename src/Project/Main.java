1bbbe4a9a84a9a026a237a4c3186d12f66a969e9
Wlad <283005@student.pwr.edu.pl>
2024-10-09 13:24:36 +0200

    Branch Stas

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -2,6 +2,7 @@ package Project;
 
 public class Main {
     public static void main(String[] args) {
+        //stas
         int size = 20; // Rozmiar tablicy
         int numberOfCreatures = 7; // Liczba istot każdego typu
         (new Simulation(size, numberOfCreatures)).run();
